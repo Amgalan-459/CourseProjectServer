3e9cd292fcbc7ed77f8be06669c8c06713141c26
Амгалан Борголов <amborgolov007@gmail.com>
2025-04-08 23:26:38 +0300

    Added crud. No testing!!!

--- CourseProjectServer/Controllers/EntetiesController.cs
+++ CourseProjectServer/Controllers/EntetiesController.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
-namespace CourseProjectServer.Controllers {
+namespace CourseProjectServer.Controllers { //потом добавить проверку по jwt токену, а потм и на posgtresql
     public class EntetiesController : Controller {
         private readonly ILogger<EntetiesController> _logger;
         private CourseDbContext dbContext;
@@ -13,10 +13,26 @@ namespace CourseProjectServer.Controllers {
             _logger = logger;
             this.dbContext = dbContext;
         }
-        internal IEnumerable<Trainee> GetAllTrainees () {
+
+
+        //дописать Exercise, ExerciseRaw
+        #region Get
+        internal async Task<IEnumerable<Trainee>> GetAllTrainees () {
             _logger.LogInformation("trainee get all");
             
-            return dbContext.Trainees.ToArray();
+            return await dbContext.Trainees.ToArrayAsync();
+        }
+
+        internal async Task<IEnumerable<Trainer>> GetAllTrainers () {
+            _logger.LogInformation("trainer get all");
+
+            return await dbContext.Trainers.ToArrayAsync();
+        }
+
+        internal async Task<IEnumerable<Workout>> GetAllWorkouts () {
+            _logger.LogInformation("workout get all");
+
+            return await dbContext.Workouts.ToArrayAsync();
         }
 
         internal async Task<Trainee> GetTrainee (int id) {
@@ -24,5 +40,74 @@ namespace CourseProjectServer.Controllers {
 
             return await dbContext.Trainees.Where(t => t.Id == id).FirstOrDefaultAsync();
         }
+
+        internal async Task<Trainer> GetTrainer (int id) {
+            _logger.LogInformation($"trainer get {id}");
+
+            return await dbContext.Trainers.Where(t => t.Id == id).FirstOrDefaultAsync();
+        }
+
+        internal async Task<Workout> GetWorkout (int id) {
+            _logger.LogInformation($"workout get {id}");
+
+            return await dbContext.Workouts.Where(t => t.Id == id).FirstOrDefaultAsync();
+        }
+
+        #endregion
+
+        //потом добавить логгер везде
+        #region Post
+        internal async Task<IResult> AddTrainee (Trainee trainee) {
+            await dbContext.Trainees.AddAsync(trainee);
+            await dbContext.SaveChangesAsync();
+            return TypedResults.Ok( trainee );
+        }
+
+        internal async Task<IResult> AddTrainer (Trainer trainer) {
+            await dbContext.Trainers.AddAsync(trainer);
+            await dbContext.SaveChangesAsync();
+            return TypedResults.Ok( trainer );
+        }
+
+        internal async Task<IResult> AddWorkout (Workout workout) {
+            await dbContext.Workouts.AddAsync(workout);
+            await dbContext.SaveChangesAsync();
+            return TypedResults.Ok( workout );
+        }
+        #endregion
+
+
+        #region Delete
+        internal async Task<IResult> DeleteTrainee (int id) {
+            Trainee? trainee = await dbContext.Trainees
+                .FirstOrDefaultAsync();
+            if (trainee is null) return TypedResults.NotFound($"Trainee с ID = {id} не найден");
+            return TypedResults.Ok( dbContext.Trainees.Remove(trainee) );
+        }
+
+        internal async Task<IResult> DeleteTrainer (int id) {
+            Trainer? trainer = await dbContext.Trainers
+                .FirstOrDefaultAsync();
+            if (trainer is null) return TypedResults.NotFound($"Trainer с ID = {id} не найден");
+            return TypedResults.Ok( dbContext.Trainers.Remove(trainer) );
+        }
+
+        internal async Task<IResult> DeleteWorkout (int id) {
+            Workout? workout = await dbContext.Workouts
+                .FirstOrDefaultAsync();
+            if (workout is null) return TypedResults.NotFound($"Workout с ID = {id} не найден");
+            return TypedResults.Ok (dbContext.Workouts.Remove(workout) );
+        }
+        #endregion
+
+
+        #region Update
+        internal async Task<IResult> UpdateTrainee (int id) {
+            Trainee? trainee = await dbContext.Trainees
+                .FirstOrDefaultAsync();
+            if (trainee is null) return TypedResults.NotFound($"Trainee с ID = {id} не найден");
+            return TypedResults.Ok( dbContext.Trainees.Update(trainee) );
+        }
+        #endregion
     }
 }
--- CourseProjectServer/Program.cs
+++ CourseProjectServer/Program.cs
@@ -1,6 +1,8 @@
 
 using CourseProjectServer.Controllers;
 using CourseProjectServer.Data.Context;
+using CourseProjectServer.Data.Entities;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CourseProjectServer {
     public class Program {
@@ -35,12 +37,56 @@ namespace CourseProjectServer {
 
             app.UseAuthorization();
 
-
+            #region MapGet
             app.MapGet("/api/trainee/all", (EntetiesController userController) =>
                 userController.GetAllTrainees());
-            app.MapGet("/api/trainee/{id: int}", (EntetiesController userController, int id) =>
+            app.MapGet("/api/trainee/{id:int}", (EntetiesController userController, int id) =>
                 userController.GetTrainee(id));
 
+            app.MapGet("/api/trainer/all", (EntetiesController userController) =>
+                userController.GetAllTrainers());
+            app.MapGet("/api/trainer/{id:int}", (EntetiesController userController, int id) =>
+                userController.GetTrainer(id));
+
+            app.MapGet("/api/workout/all", (EntetiesController userController) =>
+                userController.GetAllWorkouts());
+            app.MapGet("/api/workout/{id:int}", (EntetiesController userController, int id) =>
+                userController.GetWorkout(id));
+            #endregion
+
+
+            #region MapPost
+            app.MapPost("/api/trainee",
+                (EntetiesController controller, [FromBody] Trainee trainee) =>
+                    controller.AddTrainee(trainee));
+
+            app.MapPost("/api/trainer",
+                (EntetiesController controller, [FromBody] Trainer trainer) =>
+                    controller.AddTrainer(trainer));
+
+            app.MapPost("/api/workout",
+                (EntetiesController controller, [FromBody] Workout workout) =>
+                    controller.AddWorkout(workout));
+            #endregion
+
+
+            #region MapDelete
+            app.MapDelete("/api/trainee/{id:int}",
+                (EntetiesController controller, int id) => controller.DeleteTrainee(id));
+
+            app.MapDelete("/api/trainer/{id:int}",
+                (EntetiesController controller, int id) => controller.DeleteTrainer(id));
+
+            app.MapDelete("/api/workout/{id:int}",
+                (EntetiesController controller, int id) => controller.DeleteWorkout(id));
+            #endregion
+
+
+            #region MapUpdate
+            app.MapPut("/api/trainee/{id:int}",
+                (EntetiesController controller, int id) =>
+                controller.UpdateTrainee(id));
+            #endregion
 
             app.Run();
         }
